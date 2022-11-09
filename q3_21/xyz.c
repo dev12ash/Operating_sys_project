1a59cb0900cc062912109e6cdd1080f87a23d1a0
Devvrat <94753995+dev12ash@users.noreply.github.com>
2022-11-09 12:17:18 +0530

    Update xyz.c

--- q3_21/xyz.c
+++ q3_21/xyz.c
@@ -1,45 +1,32 @@
-/*Collatz conjecture algorithm executed by child process*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
 
-int main (int argc, char *argv[]) {
-	int z;
-	pid_t pid;
-	int status;
-	/* error checking */
-	if (argc != 2) {
-		fprintf(stderr, "Usage: ./collatz <positive_integer>\n");
-		return 1;
-	}
-	z = atoi(argv[1]);
-	if (z < 1) {
-		fprintf(stderr, "First argument must be a positive integer\n");
-		return 1;
-	}
-	/* fork */
-	pid = fork();
-	if (pid < 0) {
-		fprintf(stderr, "Fork failed\n");
-		return 1;
-	}
-	if (pid > 0) { /* parent process */
-		wait(&status);
-		printf("Child process completed with status %d; algorithm " 
-		       "terminated\n", status);
-	}
-	else { /* child process */
-		while (z != 1) {
-			printf("%d, ", z);
-			if (z % 2 == 0) {
-				z /= 2;
-			}
- 			else {
-				z = z * 3 + 1;
-			}
-		}
-		printf("%d.\n", z);
-	}
-	return 0;
+int main(int argc, char *argv[]){
+
+    int n;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    if(n > 0){
+        int id = fork();
+        if(id == 0){
+            printf("Child process is printing = \t");
+            int i = n;
+            while(i != 1){
+                printf("%d\t",i);
+                if(i %2 == 0)
+                    i = i/2;
+                else
+                    i = 3*i + 1;
+            }
+        }
+        else{
+            printf("Parent process waiting for child to finish...\n");
+            wait(NULL);
+        }
+        if(id == 0)
+            printf("1\n");
+    }
 }
